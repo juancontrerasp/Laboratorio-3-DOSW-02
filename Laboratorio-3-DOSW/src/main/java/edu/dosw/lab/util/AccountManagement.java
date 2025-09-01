5b5aedd768cdd89587624a95441d148deab32181
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-09-01 16:09:32 -0500

    Parte3: Juan Pablo: tests

--- Laboratorio-3-DOSW/src/main/java/edu/dosw/lab/util/AccountManagement.java
+++ Laboratorio-3-DOSW/src/main/java/edu/dosw/lab/util/AccountManagement.java
@@ -8,6 +8,10 @@ public class AccountManagement {
         return new Account(user, accountNumber, bank);
     }
 
+    public void makeDeposit(Account account, BigDecimal amount) {
+        account.addBalance(amount);
+    }
+
     public BigDecimal checkBalance(Account account) {return account.getAccountBalance();}
 
 }
